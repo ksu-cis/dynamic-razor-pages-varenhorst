e68682cba4fccbd54e1e08da8a20b8c4d12b4a54
varenhorst <apvarenh@ksu.edu>
2020-05-06 17:42:58 -0500

    Commits for Dynamic Razor Pages

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -17,42 +17,83 @@ namespace Movies.Pages
         /// <summary>
         /// the current search terms.
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string SearchTerms { get; set; }
 
         /// <summary>
         /// Filtered MPAA ratings
         /// </summary>
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string[] MPAARatings { get; set; }
 
         /// <summary>
         /// The filtered genres
         /// </summary>
+        [BindProperty(SupportsGet = true)]
         public string[] Genres { get; set; }
 
         /// <summary>
         /// the min IMDB
         /// </summary>
-        public float IMDBMin { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public double? IMDBMin { get; set; }
 
         /// <summary>
         /// the max IMDB
         /// </summary>
-        public float IMDBMax { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public double? IMDBMax { get; set; }
 
 
 
-        public void OnGet(double? IMDBMin, double? IMDBMax)
+        public void OnGet()
         {
-           
-            //This does not work.
-            this.IMDBMin = IMDBMin;
-            this.IMDBMax = IMDBMax;
-            Movies = MovieDatabase.Search(SearchTerms);
-            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings); 
-            //Movies = MovieDatabase.FilterByGenre(Movies, Genres); 
-            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
+            Movies = MovieDatabase.All;
+
+            //Search movie title for the search terms.
+            if(SearchTerms != null)
+            {
+                Movies = Movies.Where(movie => movie.Title != null && movie.Title.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase));
+            }
+
+            //filter through MPAARatings
+            if(MPAARatings != null && MPAARatings.Length!=0)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.MPAARating != null && MPAARatings.Contains(movie.MPAARating)
+                );
+            }
+
+            //Filter through genres
+            if (Genres != null && Genres.Length != 0)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.MajorGenre != null && Genres.Contains(movie.MajorGenre)
+                );
+            }
+
+            // Filter through IMDB Ratings
+            if (IMDBMin == null && IMDBMax != null)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.IMDBRating != null && IMDBMax >= movie.IMDBRating 
+                );
+            }
+
+            if (IMDBMin != null && IMDBMax == null)
+            {
+                Movies = Movies.Where(movie =>
+                    movie.IMDBRating != null && IMDBMin <= movie.IMDBRating
+                );
+            }
+
+            if(IMDBMin != null && IMDBMax != null)
+            {
+                Movies = Movies.Where(movie =>
+
+                   movie.IMDBRating != null && IMDBMin <= movie.IMDBRating && IMDBMax >= movie.IMDBRating
+                );
+            }
         }
 
    
